3d1e7be6c51c630d55fedabfa23ed3758d95c9d7
NKabiru <njengakabiru@gmail.com>
2019-11-22 16:07:39 +0300

    Experiment with image html element

--- js/ocr.js
+++ js/ocr.js
@@ -6,16 +6,16 @@ const worker = createWorker({
 
 const result = document.getElementById('result');
 
-let recognize = async ({target: {files}}) => {
+let recognize = async (imageElement) => {
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize();
 
-    const { data: { text } } = await worker.recognize(files[0]);
+    const { data: { text } } = await worker.recognize(imageElement);
 
     console.log(text);
     result.innerText = text;
 };
 
-const uploader = document.getElementById('uploader');
-uploader.addEventListener('change', recognize);
+const image = document.getElementById('url-image');
+recognize(image);
